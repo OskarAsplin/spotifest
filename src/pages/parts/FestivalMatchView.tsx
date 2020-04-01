46b550e687669229ac8314d6b0b0dfa5f9809a47
Oskar Asplin <oskarasplin@gmail.com>
2020-04-01 22:10:43 +0200

    ON-8 add info icon for matching algorithm

--- src/pages/parts/FestivalMatchView.tsx
+++ src/pages/parts/FestivalMatchView.tsx
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import {AppState, DispatchProps, FestivalMatch} from "../../redux/types";
 import {connect} from "react-redux";
 import {createStyles, MuiThemeProvider, Theme} from "@material-ui/core";
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import lightBlue from "@material-ui/core/colors/lightBlue";
 import pink from "@material-ui/core/colors/pink";
+import indigo from "@material-ui/core/colors/indigo";
 import {Model} from "../../redux/types";
 //import useMediaQuery from "@material-ui/core/useMediaQuery/useMediaQuery";
 import Typography from '@material-ui/core/Typography';
@@ -14,6 +15,9 @@ import 'react-circular-progressbar/dist/styles.css';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Switch from '@material-ui/core/Switch';
+import Tooltip from '@material-ui/core/Tooltip';
+import InfoIcon from '@material-ui/icons/Info';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -42,6 +46,17 @@ const useStyles = makeStyles((theme: Theme) =>
 		box: {
 			width: '80%',
 			maxWidth: '700px'
+		},
+		button: {
+			textTransform: 'none',
+			padding: theme.spacing(0),
+			fontSize: '18px',
+			"&:hover": {
+				backgroundColor: "transparent",
+			}
+		},
+		invisibleButton: {
+			display: 'none'
 		}
 	}),
 );
@@ -52,11 +67,23 @@ interface StoreProps {
 
 type Props = DispatchProps & StoreProps;
 
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 320,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
+
 const FestivalMatchView: React.FC<Props> = (props: Props) => {
 
 	// const smallScreen = useMediaQuery('(max-width:610px)');
 
 	const [genreArtistChecked, setGenreArtistChecked] = useState<boolean>(false);
+
+	const thememode = props.model.thememode;
 
 	const lightBluePinkMuiTheme = createMuiTheme({
         palette: {
@@ -70,7 +97,7 @@ const FestivalMatchView: React.FC<Props> = (props: Props) => {
                 main: pink[500],
                 dark: pink[700]
             },
-            type: props.model.thememode
+            type: thememode
         }
     });
 
@@ -80,13 +107,52 @@ const FestivalMatchView: React.FC<Props> = (props: Props) => {
 		setGenreArtistChecked(event.target.checked);
 	};
 
+	const handleGenreArtistChangeOnText = (artistActive: boolean) => {
+		setGenreArtistChecked(artistActive);
+	};
+
+	const capitalizeFirstLetter = (string: string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
 	return (
 		<Box className={classes.box}>
-			<Box className={classes.alignCenter}>
-				Genre
-				<Switch checked={genreArtistChecked} color="default" onChange={handleGenreArtistChange} name="checkedC" />
-				Artist
-			</Box>
+			<Grid component="label" container alignItems="center" spacing={1}>
+				<Grid item xs={1}/>
+				<Grid item xs={10}>
+					<div className={classes.alignCenter}>
+						<MuiThemeProvider theme={lightBluePinkMuiTheme}>
+							<Button hidden className={classes.invisibleButton} />
+							<Button disableRipple disableElevation className={classes.button}
+							color={genreArtistChecked ? 'default' : 'primary'}
+							onClick={() => handleGenreArtistChangeOnText(false)}>
+								Genre
+							</Button>
+							<Switch checked={genreArtistChecked} color="default" onChange={handleGenreArtistChange} name="checkedC" />
+							<Button disableRipple disableElevation className={classes.button}
+							color={genreArtistChecked ? 'primary' : 'default'}
+							onClick={() => handleGenreArtistChangeOnText(true)}>
+								Artist
+							</Button>
+						</MuiThemeProvider>
+					</div>
+				</Grid>
+				<Grid item xs={1}>
+					<HtmlTooltip placement="right-start" interactive
+						title={
+							<React.Fragment>
+								<Typography color="inherit" variant="h6">Matching algorithm</Typography>
+								<Typography color="inherit" variant="subtitle1"><em>{"Genres"}</em></Typography>
+								{'Genre matching takes the genres of your top 50 artists and compares it to the genres of our registered festivals.'}
+								<Typography color="inherit" variant="subtitle1"><em>{"Artists"}</em></Typography>
+								{'Artist matching checks if any of your top 50 artists have been to our registered festivals the last 1-3 years.'}
+							</React.Fragment>
+						}
+					>
+						<InfoIcon color="primary" style={{ fill: thememode === 'light' ? indigo[500] : '#fcfcfe' }}/>
+					</HtmlTooltip>
+				</Grid>
+			</Grid>
 			{props.model.festivalMatches
 				.sort((a, b) => (genreArtistChecked ? 
 					(a.matching_percent_artists < b.matching_percent_artists) : 
@@ -97,17 +163,17 @@ const FestivalMatchView: React.FC<Props> = (props: Props) => {
 					if (genreArtistChecked) {
 						matching_percent = Math.ceil(festival.matching_percent_artists);
 						matching_text = festival.matching_artists.length > 0 ?
-						'Matching artists: ' + festival.matching_artists.slice(0, 5).join(", ")
+						capitalizeFirstLetter(festival.matching_artists.slice(0, 5).join(", "))
 						: 'No artists match with this festival';
 					} else {
 						matching_percent = Math.ceil(festival.matching_percent_genres);
 						matching_text = festival.matching_genres.length > 0 ?
-						'Matching genres: ' + festival.matching_genres.slice(0, 5).join(", ")
+						capitalizeFirstLetter(festival.matching_genres.slice(0, 5).join(", "))
 						: 'No genres match with this festival';
 					}
-					const pathColor = props.model.thememode === 'light' ? '#3FBF3F' : '#3de53d';
-					const textColor = props.model.thememode === 'light' ? '#3FBF3F' : '#3de53d';
-					const trailColor = props.model.thememode === 'light' ? '#d6d6d6' : 'rgba(104, 104, 104)';
+					const pathColor = thememode === 'light' ? '#3FBF3F' : '#3de53d';
+					const textColor = thememode === 'light' ? '#3FBF3F' : '#3de53d';
+					const trailColor = thememode === 'light' ? '#d6d6d6' : 'rgba(104, 104, 104)';
 					return (
 						<div className={classes.root} key={festival.name}>
 							<MuiThemeProvider theme={lightBluePinkMuiTheme}>
