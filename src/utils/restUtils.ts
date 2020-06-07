c3a4b1dcea4e3dbd7d691e64be9c2691b9ff5a89
Oskar Asplin <oskarasplin@gmail.com>
2020-06-08 00:17:23 +0200

    second attempt to access digitalocean

--- src/utils/restUtils.ts
+++ src/utils/restUtils.ts
@@ -9,7 +9,7 @@ export function getApiBaseUrl(): string {
     if (erDev()) {
         return "http://127.0.0.1:8000";
     } else {
-        return "157.230.25.202:8000"
+        return "http://157.230.25.202:8000"
     }
 }
 
