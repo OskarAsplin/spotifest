3c09ff8c41038753880f5c27fc6ee3c4eafa50dd
Oskar Asplin <oskarasplin@gmail.com>
2020-06-12 18:14:37 +0200

    more experimenting with different touchDelay for tooltip

--- src/pages/parts/FestivalMatchItem.tsx
+++ src/pages/parts/FestivalMatchItem.tsx
@@ -250,7 +250,7 @@ const FestivalMatchItem: React.FC<Props> = (props: Props) => {
                         {showMatching &&
                             <div>
                                 <Box className={classes.toolTip}>
-                                    <HtmlTooltip placement="left-start" interactive enterTouchDelay={200} leaveTouchDelay={3000}
+                                    <HtmlTooltip placement="left-start" interactive disableTouchListener enterTouchDelay={0} leaveTouchDelay={3000}
                                         title={
                                             <React.Fragment>
                                                 <Typography color="inherit" variant={bigScreen ? 'subtitle2' : 'body2'}>{'Genres: ' + Math.ceil(festival.matching_percent_genres) + '%'}</Typography>
