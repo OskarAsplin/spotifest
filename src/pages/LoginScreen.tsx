85ef0a69132684451de9162be8a00b23de4b5674
Oskar Asplin <oskarasplin@gmail.com>
2020-08-04 17:13:11 +0200

    Make login button and title positions absolute

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -15,7 +15,7 @@ import { lightBlue, pink } from "@material-ui/core/colors";
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            position: 'fixed',
+            position: 'absolute',
             display: 'flex',
             flexDirection: 'column',
             '@media (min-width: 400px)': {
@@ -30,7 +30,7 @@ const useStyles = makeStyles((theme: Theme) =>
             width: '100%',
         },
         root1: {
-            position: 'fixed',
+            position: 'absolute',
             display: 'flex',
             flexDirection: 'column',
             '@media (min-width: 334px)': {
