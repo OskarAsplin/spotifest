704c2494ac24163f16cffdc6911c2e9e1b2b7615
Oskar Asplin <oskarasplin@gmail.com>
2022-12-03 15:21:39 +0100

    Fix date formatting to local ISO format

--- src/containers/FestivalMatchesContainer.utils.ts
+++ src/containers/FestivalMatchesContainer.utils.ts
@@ -1,5 +1,5 @@
 import { MatchCriteria, MatchRequest } from '../redux/types';
-import { getShortDateISOString } from '../utils/utils';
+import { getLocalISODate } from '../utils/utils';
 import { mapToArtistMinimal } from '../utils/mappers';
 
 export const createMatchRequest = ({
@@ -12,17 +12,15 @@ export const createMatchRequest = ({
   countries,
   states,
 }: MatchCriteria): MatchRequest => {
-  dateFrom.setUTCHours(0);
   dateFrom.setDate(1); // Frist day of month
-  dateTo.setUTCHours(0);
   dateTo.setMonth(dateTo.getMonth() + 1, 0); // Last day of month
   return {
     artists: artists.map(mapToArtistMinimal),
     genres: artists.flatMap((artist) => artist.genres),
     numTracks: numTracks,
     isTopArtists: isTopArtists,
-    dateFrom: getShortDateISOString(dateFrom),
-    dateTo: getShortDateISOString(dateTo),
+    dateFrom: getLocalISODate(dateFrom),
+    dateTo: getLocalISODate(dateTo),
     continents: continents ?? [],
     countries: countries ?? [],
     states: states ?? [],
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,8 +3,13 @@ import countries_list from 'countries-list/dist/data.json';
 import { Area } from '../redux/types';
 import { europeanRegions, regionMap, usRegions } from './regionUtils';
 
-export const getShortDateISOString = (date: Date) =>
-  date.toISOString().substring(0, date.toISOString().search('T'));
+export const getLocalISODate = (date: Date) => {
+  const year = date.getFullYear().toString().padStart(4, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const dt = date.getDate().toString().padStart(2, '0');
+
+  return `${year}-${month}-${dt}`;
+};
 
 export const getIconPicture = (images: SpotifyApi.ImageObject[]): string => {
   let picture = '';
