825964f6612b965b166c5b40bb3c22ac6c07285e
Oskar Asplin <oskarasplin@gmail.com>
2023-08-27 16:41:54 +0200

    Update max matching date to end of next year

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const MATCHING_MIN_DATE = new Date(2021, 0, 1);
-export const MATCHING_MAX_DATE = new Date(2023, 11, 31);
+export const MATCHING_MAX_DATE = new Date(2024, 11, 31);
 
 export const INITIAL_FROM_DATE = new Date();
 export const INITIAL_TO_DATE = MATCHING_MAX_DATE;
