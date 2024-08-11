43f31af058691aa9ad9074a2dd602253298a8a98
Oskar Asplin <oskarasplin@gmail.com>
2024-08-11 17:32:57 +0200

    Upgrade storybook packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -10,10 +11,16 @@ const config: StorybookConfig = {
       name: '@storybook/addon-styling',
       options: {},
     },
+    '@chromatic-com/storybook',
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 };
 
