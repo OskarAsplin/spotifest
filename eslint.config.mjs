632662b98a2d9853c541f51bccdec06dde4bb502
Oskar Asplin <oskarasplin@gmail.com>
2024-08-11 17:39:49 +0200

    Fix new eslint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,72 @@
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier",
+), {
+    plugins: {
+        react,
+        "react-hooks": fixupPluginRules(reactHooks),
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+            Atomics: "readonly",
+            SharedArrayBuffer: "readonly",
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2018,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: {},
+        },
+    },
+
+    rules: {
+        "no-unused-vars": "off",
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+        }],
+
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/display-name": "off",
+    },
+}];
