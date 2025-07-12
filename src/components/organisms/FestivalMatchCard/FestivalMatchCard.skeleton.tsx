9f69596329da32e4149c956963b52de4dddb75a1
Oskar Asplin <oskarasplin@gmail.com>
2025-07-12 22:47:16 +0200

    Adjust padding FestivalMatchCardSkeleton

--- src/components/organisms/FestivalMatchCard/FestivalMatchCard.skeleton.tsx
+++ src/components/organisms/FestivalMatchCard/FestivalMatchCard.skeleton.tsx
@@ -26,7 +26,7 @@ export const FestivalMatchCardSkeleton = ({
     <Card className="mb-4 w-full pt-1 pb-0 shadow-lg sm:pt-2">
       <CardContent className="px-2 sm:px-4">
         <div className="pb-2" />
-        <div className="px-4">
+        <div className="px-2 sm:px-4">
           <div className="flex w-full flex-row justify-between">
             <div>
               <Skeleton className="border-primary hover:bg-primary/5 mb-3.5 h-11.5 w-40 border-dotted px-2 sm:mb-3 sm:h-14 sm:w-70" />
@@ -49,10 +49,10 @@ export const FestivalMatchCardSkeleton = ({
               <StyledAvatarContainerDiv key={i} />
             ))}
         </ArtistBox>
-        <div className="mt-2 mb-2 flex w-full items-center gap-4 sm:mt-4">
-          <Separator className="mx-2 flex-1" />
+        <div className="mt-2 mb-2 flex w-full items-center gap-3 px-2 sm:mt-4">
+          <Separator className="flex-1" />
           <Skeleton className="my-1.5 h-4 w-50" />
-          <Separator className="mx-2 flex-1" />
+          <Separator className="flex-1" />
         </div>
         <ArtistBox>
           {mockPopularArtists.slice(0, maxArtistsInWidth).map((_, i) => (
