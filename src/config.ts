6fc02d652877f68e81c317cfc3fb091fd3e5df2d
Oskar Asplin <oskarasplin@gmail.com>
2025-11-07 22:59:25 +0100

    Make min date and max date dynamic based on the time of the year

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,12 @@
 import dayjs from 'dayjs';
 
-export const MATCHING_MIN_DATE = dayjs('2021-01-01');
-export const MATCHING_MAX_DATE = dayjs('2025-12-31');
-export const DATE_RANGE_YEAR_OPTIONS = [2023, 2024, 2025];
+const currentYear = dayjs().year();
+const isAfterJuly = dayjs().month() >= 6; // July is month 6 (0-indexed)
+const maxYear = isAfterJuly ? currentYear + 1 : currentYear;
+const minYear = maxYear - 3;
+export const MATCHING_MIN_DATE = dayjs(`${minYear}-01-01`);
+export const MATCHING_MAX_DATE = dayjs(`${maxYear}-12-31`);
+export const DATE_RANGE_YEAR_OPTIONS = [maxYear - 2, maxYear - 1, maxYear];
 
 export const INITIAL_FROM_DATE = dayjs();
 export const INITIAL_TO_DATE = MATCHING_MAX_DATE;
